4289dabec0f9229a84359bc798e865422a63dedc
pixel <pixel@chrissx.de>
2023-03-06 13:58:03 +0100

    skeleton

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+
+function Root() {
+return <></>;
+}
+
+ReactDOMClient.createRoot(document.getElementById("root")!).render(<Root />);
