65bc6fb251691c4c5d987b4c71273b6029c5207d
pixel <pixel@chrissx.de>
2023-03-08 17:52:51 +0100

    better M names, questionable UTS

--- src/Thread.tsx
+++ src/Thread.tsx
@@ -1,4 +1,4 @@
-import { multiply, subtract, unit, Unit } from "mathjs";
+import { evaluate, multiply, round, subtract, unit, Unit } from "mathjs";
 
 export type Thread = {
     name: string,
@@ -85,8 +85,97 @@ const mFine = {
 export function M(diameter: number, pitch: number | "coarse" | "fine" = "coarse"): Thread {
     const p = pitch == "coarse" ? mCoarse[diameter] : "fine" ? mFine[diameter] : pitch;
     return {
-        name: "M" + diameter + (p == mCoarse[diameter] ? "" : "×" + p),
+        name: pitch == "coarse" ? "M" + diameter : pitch == "fine" ? "MF" + diameter : "M" + diameter + "×" + p,
         diameter: unit(diameter, "mm"),
-        pitch: unit(p, "mm")
+        pitch: unit(p, "mm"),
+    };
+}
+
+const unc = {
+    0.0730: 64,
+    0.0860: 56,
+    0.0990: 48,
+    0.1120: 40,
+    0.1250: 40,
+    0.1380: 32,
+    0.1640: 32,
+    0.1900: 24,
+    0.2160: 24,
+    0.2500: 20,
+    0.3125: 18,
+    0.3750: 16,
+    0.4375: 14,
+    0.5000: 13,
+    0.5625: 12,
+    0.6250: 11,
+    0.7500: 10,
+    0.8750: 9,
+    1.0000: 8,
+    1.1250: 7,
+    1.2500: 7,
+    1.3750: 6,
+    1.5000: 6,
+    1.7500: 5,
+    2.0000: 4.5,
+    2.2500: 4.5,
+    2.5000: 4,
+    2.7500: 4,
+    3.0000: 4,
+    3.2500: 4,
+    3.5000: 4,
+    3.7500: 4,
+    4.0000: 4,
+};
+
+const unf = {
+    0.0600: 80,
+    0.0730: 72,
+    0.0860: 64,
+    0.0990: 56,
+    0.1120: 48,
+    0.1250: 44,
+    0.1380: 40,
+    0.1640: 36,
+    0.1900: 32,
+    0.2160: 28,
+    0.2500: 28,
+    0.3125: 24,
+    0.3750: 24,
+    0.4375: 20,
+    0.5000: 20,
+    0.5625: 18,
+    0.6250: 18,
+    0.7500: 16,
+    0.8750: 14,
+    1.0000: 12,
+    1.1250: 12,
+    1.2500: 12,
+    1.3750: 12,
+    1.5000: 12,
+};
+
+const unef = {
+    0.2160: 32,
+    0.2500: 32,
+    0.3125: 32,
+    0.3750: 32,
+    0.4375: 28,
+    0.5000: 28,
+    0.5625: 24,
+    0.6250: 24,
+    0.7500: 20,
+    0.8750: 20,
+    1.0000: 20,
+};
+
+export function uts(diameter: string, tpi: number | "unc" | "coarse" | "unf" | "fine" | "unef" | "extrafine" = "unc"): Thread {
+    const ed = diameter.includes("#00") ? "#-" + (diameter.split("0").length - 2) : diameter;
+    const d = round(evaluate(ed.replace("#", "0.060+0.013*")), 10);
+    const t = tpi == "unc" || tpi == "coarse" ? unc[d] : tpi == "unf" || tpi == "fine" ? unf[d] : tpi == "unef" || tpi == "extrafine" ? unef[d] : tpi;
+    console.log("UTS diameter: " + d);
+    return {
+        name: diameter + "-" + t,
+        diameter: unit(d, "in"),
+        pitch: unit(1 / t, "in"),
     };
 }
