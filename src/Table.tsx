51877192b74d2475dda5e836d5e53bbf52501200
pixel <pixel@chrissx.de>
2023-04-06 08:37:26 +0200

    gray out parts of the table to make it easier to read

--- src/Table.tsx
+++ src/Table.tsx
@@ -5,13 +5,25 @@ import { minorDiameter, pitchDiameter, Thread } from "./Thread";
 export type LengthUnit = "mm" | "inch";
 export type PitchUnit = "tpmm" | "tpi";
 
+export type Settings = {
+    lengthUnit: LengthUnit,
+    pitchUnit: PitchUnit,
+    threads: Thread[],
+};
+
+export type PartialSettings = {
+    lengthUnit?: LengthUnit,
+    pitchUnit?: PitchUnit,
+    threads?: Thread[],
+};
+
 function convert(u: Unit, to: LengthUnit | PitchUnit): number {
     if(to == "tpmm") return 1 / u.toNumber("mm");
     if(to == "tpi") return 1 / u.toNumber("inch");
     return u.toNumber(to);
 }
 
-export default function Table({ lengthUnit, pitchUnit, threads }: { lengthUnit: LengthUnit, pitchUnit: PitchUnit, threads: Thread[] }) {
+export default function Table({ lengthUnit, pitchUnit, threads }: Settings) {
     const rnd = (x: Unit, unit: LengthUnit | PitchUnit = lengthUnit) => round(convert(x, unit), 4);
     return (
         <table>
@@ -19,13 +31,13 @@ export default function Table({ lengthUnit, pitchUnit, threads }: { lengthUnit: 
                 <tr>
                     <th>Thread</th>
                     <th>D<sub>maj</sub> / {lengthUnit}</th>
-                    <th>D<sub>p</sub> / {lengthUnit}</th>
-                    <th>D<sub>min</sub> / {lengthUnit}</th>
+                    <th className="unimportant">D<sub>p</sub> / {lengthUnit}</th>
+                    <th className="unimportant">D<sub>min</sub> / {lengthUnit}</th>
                     <th>P / {pitchUnit}</th>
                     <th>P / {lengthUnit}</th>
-                    <th><sup>P</sup>&frasl;<sub>2</sub> / {lengthUnit}</th>
-                    <th><sup>P</sup>&frasl;<sub>4</sub> / {lengthUnit}</th>
-                    <th><sup>P</sup>&frasl;<sub>8</sub> / {lengthUnit}</th>
+                    <th className="unimportant"><sup>P</sup>&frasl;<sub>2</sub> / {lengthUnit}</th>
+                    <th className="unimportant"><sup>P</sup>&frasl;<sub>4</sub> / {lengthUnit}</th>
+                    <th className="unimportant"><sup>P</sup>&frasl;<sub>8</sub> / {lengthUnit}</th>
                 </tr>
             </thead>
             <tbody>
@@ -33,13 +45,13 @@ export default function Table({ lengthUnit, pitchUnit, threads }: { lengthUnit: 
                     <tr key={x.name}>
                         <td>{x.name}</td>
                         <td>{rnd(x.diameter)}</td>
-                        <td>{rnd(pitchDiameter(x))}</td>
-                        <td>{rnd(minorDiameter(x))}</td>
+                        <td className="unimportant">{rnd(pitchDiameter(x))}</td>
+                        <td className="unimportant">{rnd(minorDiameter(x))}</td>
                         <td>{rnd(x.pitch, pitchUnit)}</td>
                         <td>{rnd(x.pitch)}</td>
-                        <td>{rnd(divide(x.pitch, 2))}</td>
-                        <td>{rnd(divide(x.pitch, 4))}</td>
-                        <td>{rnd(divide(x.pitch, 8))}</td>
+                        <td className="unimportant">{rnd(divide(x.pitch, 2))}</td>
+                        <td className="unimportant">{rnd(divide(x.pitch, 4))}</td>
+                        <td className="unimportant">{rnd(divide(x.pitch, 8))}</td>
                     </tr>
                 )}
             </tbody>
